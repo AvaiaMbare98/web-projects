4e4e78189c5577995139951ba6a5a81457b49c92
AvaiaMbare98 <giograsso98@gmail.com>
2025-08-19 11:18:23 +0200

    Added button for reset (not working yet)

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,6 +1,10 @@
 const bigdiv = document.querySelector(".container");
+const SCREEN_WIDTH = 960;
 
 function applyStyle(target){
+    if(target === bigdiv){
+        return;
+    }
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
@@ -10,11 +14,8 @@ function applyStyle(target){
 
 for(let i = 0; i < 256; i++){ //256 perché è 16x16
     const div = document.createElement("div");
-    div.classList.add("square");
+    // div.classList.add("square");
     bigdiv.appendChild(div);
 }
 
-for(let i = 0; i < 256; i++){
-    
-    bigdiv.addEventListener("mouseover", (e) => applyStyle(e.target));
-}
+bigdiv.addEventListener("mouseover", (e) => applyStyle(e.target));
