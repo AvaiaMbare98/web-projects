b1d173a4372dcfd0535c2dfe2b09aa1a1551a439
AvaiaMbare98 <giograsso98@gmail.com>
2025-08-21 12:10:22 +0200

    Added pointers to elements in document

--- calculator/script.js
+++ calculator/script.js
@@ -1,17 +1,45 @@
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const zero = document.querySelector(".zero");
+const plus = document.querySelector(".plus");
+const minus = document.querySelector(".minus");
+const multiply = document.querySelector(".multiply");
+const divide = document.querySelector(".divide");
+
 function add(a, b){
-    return a + b;
+    return (+a) + (+b);
 }
 
 function subtract(a, b){
-    return a - b;
+    return (+a) - (+b);
 }
 
 function multiply(a, b){
-    return a * b;
+    return (+a) * (+b);
 }
 
 function divide(a, b){
-    return a / b;
+    return (+a) / (+b);
+}
+
+function operate(a, b, operator){
+    switch(operator){
+        case '+':
+            return add(a,b);
+        case '-':
+            return subtract(a,b);
+        case 'x':
+            return multiply(a,b);
+        case '/':
+            return divide(a,b);
+    }
 }
 
 function main(){
@@ -19,6 +47,6 @@ function main(){
     let op2 = 0;
     let operator;
 
-    
+
 }
 main();
