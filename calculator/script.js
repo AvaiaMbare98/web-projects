d138a0170e6dd38985fe1eb219ab3bcf67809651
AvaiaMbare98 <giograsso98@gmail.com>
2025-08-21 11:53:43 +0200

    Added keypad

---
+++ calculator/script.js
@@ -0,0 +1,24 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function main(){
+    let op1 = 0;
+    let op2 = 0;
+    let operator;
+
+    
+}
+main();
